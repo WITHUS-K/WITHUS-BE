0e674b721340a520fd5b6fa1e4be88a57b57e11e
shash0423 <shash042319@naver.com>
2025-04-05 22:09:53 +0900

    feat: Swagger 연동 테스트를 위한 Config 약식 구성 #1

---
+++ src/main/java/KUSITMS/WITHUS/global/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package KUSITMS.WITHUS.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final String[] allowedUrls = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers(allowedUrls).permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+}
