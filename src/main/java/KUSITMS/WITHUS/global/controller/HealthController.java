9bd15df5301cd9da22b1a44bd5ce44d5702a9b9a
shash0423 <shash042319@naver.com>
2025-04-09 15:11:25 +0900

    docs: HealthController Description 추가 #5

--- src/main/java/KUSITMS/WITHUS/global/controller/HealthController.java
+++ src/main/java/KUSITMS/WITHUS/global/controller/HealthController.java
@@ -1,9 +1,11 @@
 package KUSITMS.WITHUS.global.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "서버 상태 체크를 위한 API")
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck() {
