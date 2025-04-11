3dc3045c9c811d867962be87f08dac9745914a8c
EunjinWoo <esther0904@naver.com>
2025-04-11 12:13:37 +0900

    feat: 커스텀 로그인 설정 #6

---
+++ src/main/java/KUSITMS/WITHUS/domain/auth/controller/AdminController.java
@@ -0,0 +1,13 @@
+package KUSITMS.WITHUS.domain.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+        return "admin";
+    }
+}
--- src/main/java/KUSITMS/WITHUS/global/config/SecurityConfig.java
+++ src/main/java/KUSITMS/WITHUS/global/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,9 +23,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers(allowedUrls).permitAll()
-                .anyRequest().authenticated());
+//        http
+//                .authorizeHttpRequests((auth) -> auth
+//                        .requestMatchers(allowedUrls).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll         // 누구나 접근 가능하게
+//                )
+//                .logout(LogoutConfigurer::permitAll)
+//                .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
+
+        http
+                .csrf((auth) -> auth.disable());
 
         return http.build();
     }
