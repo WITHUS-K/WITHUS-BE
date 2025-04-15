000705d30c0539d7ac4dc0d05d7eb6f278e5b954
shash0423 <shash042319@naver.com>
2025-04-15 15:29:18 +0900

    feat: Evaluation 도메인 Entity 생성 #10

---
+++ src/main/java/KUSITMS/WITHUS/domain/evaluation/entity/Evaluation.java
@@ -0,0 +1,36 @@
+package KUSITMS.WITHUS.domain.evaluation.entity;
+
+import KUSITMS.WITHUS.domain.application.entity.Application;
+import KUSITMS.WITHUS.domain.user.entity.User;
+import KUSITMS.WITHUS.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "EVALUATION")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Evaluation extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EVALUATION_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private int score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "APPLICATION_ID", nullable = false)
+    private Application application;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CRITERIA_ID", nullable = false)
+    private EvaluationCriteria criteria;
+}
