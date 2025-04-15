6365f83b781fc618e92198fa07bf2e7c7405b6e7
shash0423 <shash042319@naver.com>
2025-04-15 15:27:00 +0900

    feat: Application 도메인 Entity 생성 #10

---
+++ src/main/java/KUSITMS/WITHUS/domain/application/entity/ApplicationQuestion.java
@@ -0,0 +1,31 @@
+package KUSITMS.WITHUS.domain.application.entity;
+
+import KUSITMS.WITHUS.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "APPLICATION_QUESTION")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApplicationQuestion extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "QUESTION_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEMPLATE_ID", nullable = false)
+    private ApplicationTemplate template;
+
+    @Column(nullable = false)
+    private String Content;
+
+    public void associateTemplate(ApplicationTemplate template) {
+        this.template = template;
+    }
+
+}
---
+++ src/main/java/KUSITMS/WITHUS/domain/application/entity/ApplicationTemplate.java
@@ -0,0 +1,39 @@
+package KUSITMS.WITHUS.domain.application.entity;
+
+import KUSITMS.WITHUS.domain.recruitment.entity.Recruitment;
+import KUSITMS.WITHUS.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "APPLICATION_TEMPLATE")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApplicationTemplate extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TEMPLATE_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RECRUITMENT_ID", nullable = false)
+    private Recruitment recruitment;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "template", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ApplicationQuestion> questions = new ArrayList<>();
+
+    public void addQuestion(ApplicationQuestion question) {
+        this.questions.add(question);
+        question.associateTemplate(this);
+    }
+}
