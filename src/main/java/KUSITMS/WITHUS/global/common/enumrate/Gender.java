ace946c9b5e835b5f99b6fc8d14ef1b1e0370eed
shash0423 <shash042319@naver.com>
2025-04-15 15:29:45 +0900

    feat: Gender Enum 생성 #10

---
+++ src/main/java/KUSITMS/WITHUS/global/common/enumrate/Gender.java
@@ -0,0 +1,14 @@
+package KUSITMS.WITHUS.global.common.enumrate;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+
+    MALE("남"),
+    FEMALE("여");
+
+    private final String key;
+}
