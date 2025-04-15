800b755155168ef6251f7535bd13cc07f906bddd
shash0423 <shash042319@naver.com>
2025-04-15 15:28:30 +0900

    feat: User 도메인 Entity 생성 #10

--- src/main/java/KUSITMS/WITHUS/domain/user/entity/User.java
+++ src/main/java/KUSITMS/WITHUS/domain/user/entity/User.java
@@ -1,36 +1,75 @@
 package KUSITMS.WITHUS.domain.user.entity;
 
+import KUSITMS.WITHUS.domain.application.entity.Comment;
 import KUSITMS.WITHUS.domain.user.enumerate.Role;
+import KUSITMS.WITHUS.global.common.BaseEntity;
+import KUSITMS.WITHUS.global.common.enumrate.Gender;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
+@Table(name = "USER")
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "USER_ID")
+    private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "EMAIL", nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "PASSWORD", nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "ROLE", nullable = false)
     private Role role;
 
-    @Builder
-    public User(String email, String password, Role role) {
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(name = "GENDER", nullable = false)
+    private Gender gender;
+
+    @Column(name = "BIRTH_DATE")
+    private LocalDate birthDate;
+
+    @Column(name = "PHONE_NUMBER", nullable = false)
+    private String phoneNumber;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserOrganization> userOrganizations = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
     public void changePassword(String newPassword) {
         this.password = newPassword;
+    }
+
+    public void changeRole(Role role) {
+        this.role = role;
+    }
+
+    public void addUserOrganization(UserOrganization userOrganization) {
+        this.userOrganizations.add(userOrganization);
+        userOrganization.associateUser(this);
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        comment.associateUser(this);
     }
 }
