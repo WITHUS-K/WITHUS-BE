eea3d8aadd197aecb27b2b1715747067fd399d44
shash0423 <shash042319@naver.com>
2025-04-15 16:49:29 +0900

    feat: CurrentUser Annotation 생성 #12

--- src/main/java/KUSITMS/WITHUS/domain/user/service/UserService.java
+++ src/main/java/KUSITMS/WITHUS/domain/user/service/UserService.java
@@ -1,7 +1,9 @@
 package KUSITMS.WITHUS.domain.user.service;
 
 import KUSITMS.WITHUS.domain.user.dto.UserRequestDTO;
+import KUSITMS.WITHUS.domain.user.entity.User;
 
 public interface UserService {
+    User getUserByEmail(String email);
     void joinProcess(UserRequestDTO.Join request);
 }
