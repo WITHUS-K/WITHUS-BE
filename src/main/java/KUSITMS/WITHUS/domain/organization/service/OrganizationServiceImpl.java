e77985919d10127da829947621c0f64ee6e308d4
shash0423 <shash042319@naver.com>
2025-04-17 09:09:24 +0900

    docs: 주석 추가 #16

--- src/main/java/KUSITMS/WITHUS/domain/organization/service/OrganizationServiceImpl.java
+++ src/main/java/KUSITMS/WITHUS/domain/organization/service/OrganizationServiceImpl.java
@@ -17,6 +17,11 @@ public class OrganizationServiceImpl implements OrganizationService {
 
     private final OrganizationRepository organizationRepository;
 
+    /**
+     * 조직 생성
+     * @param request 조직 이름을 입력받습니다.
+     * @return 생성된 조직 정보(ID, 조직명) 반환
+     */
     @Override
     @Transactional
     public OrganizationResponseDTO.Create create(OrganizationRequestDTO.Create request) {
@@ -25,6 +30,11 @@ public class OrganizationServiceImpl implements OrganizationService {
         return OrganizationResponseDTO.Create.from(organizationRepository.save(organization));
     }
 
+    /**
+     * ID로 조직 단건 조회
+     * @param id 조회할 조직의 ID 입력
+     * @return 조직 상세 정보 반환
+     */
     @Override
     public OrganizationResponseDTO.Detail getById(Long id) {
         Organization organization = organizationRepository.getById(id);
@@ -35,6 +45,12 @@ public class OrganizationServiceImpl implements OrganizationService {
         );
     }
 
+    /**
+     * 조직 정보 수정
+     * @param id 수정할 조직의 ID
+     * @param request 수정 정보(조직명)
+     * @return 수정된 조직 정보(ID, 조직명) 반환
+     */
     @Override
     @Transactional
     public OrganizationResponseDTO.Update update(Long id, OrganizationRequestDTO.Update request) {
@@ -48,6 +64,10 @@ public class OrganizationServiceImpl implements OrganizationService {
         );
     }
 
+    /**
+     * ID로 조직 삭제
+     * @param id 삭제할 조직의 ID
+     */
     @Override
     @Transactional
     public void delete(Long id) {
@@ -55,6 +75,10 @@ public class OrganizationServiceImpl implements OrganizationService {
         organizationRepository.delete(id);
     }
 
+    /**
+     * 조직 전체 조회
+     * @return 저장된 조직 전체를 반환
+     */
     @Override
     public List<OrganizationResponseDTO.Summary> getAll() {
         return organizationRepository.findAll().stream()
