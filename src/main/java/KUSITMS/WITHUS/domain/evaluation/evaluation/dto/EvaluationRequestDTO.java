c3736f51f563ea0b9f9b0df532b8c48dec94f2a2
EunjinWoo <esther0904@naver.com>
2025-05-08 14:09:33 +0900

    feat: 다중 평가 api 구현 #67

--- src/main/java/KUSITMS/WITHUS/domain/evaluation/evaluation/dto/EvaluationRequestDTO.java
+++ src/main/java/KUSITMS/WITHUS/domain/evaluation/evaluation/dto/EvaluationRequestDTO.java
@@ -1,12 +1,15 @@
 package KUSITMS.WITHUS.domain.evaluation.evaluation.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
 
 @Schema(description = "평가 관련 요청 DTO")
 public class EvaluationRequestDTO {
 
-    @Schema(description = "평가 요청 DTO")
+    @Schema(name = "EvaluationRequestDTO.Create", description = "평가 요청 DTO")
     public record Create(
             @Schema(description = "지원서 ID", example = "1")
             @NotNull Long applicationId,
@@ -17,4 +20,24 @@ public class EvaluationRequestDTO {
             @Schema(description = "점수", example = "5")
             @NotNull int score
     ) {}
+
+    @Schema(name = "EvaluationRequestDTO.BulkCreate", description = "다중 평가 요청 DTO")
+    public record BulkCreate(
+            @Schema(description = "지원서 ID", example = "1")
+            @NotNull Long applicationId,
+
+            @Schema(description = "평가 항목 리스트", required = true)
+            @NotEmpty List<EvaluationItem> evaluations
+
+    ) {
+        @Schema(name = "EvaluationRequestDTO.BulkCreate.EvaluationItem", description = "평가 항목 DTO")
+        public record EvaluationItem(
+                @Schema(description = "평가 기준 ID", example = "3")
+                @NotNull Long criteriaId,
+
+                @Schema(description = "평가 점수", example = "5")
+                @NotNull int score
+
+        ) {}
+    }
 }
