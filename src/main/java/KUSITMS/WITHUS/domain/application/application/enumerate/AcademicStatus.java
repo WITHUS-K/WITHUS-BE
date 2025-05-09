a4b8244e1195581f2f79827c61c06b061fc6b970
EunjinWoo <esther0904@naver.com>
2025-05-09 10:44:20 +0900

    feat: academic status에 유예 옵션 추가 #67

--- src/main/java/KUSITMS/WITHUS/domain/application/application/enumerate/AcademicStatus.java
+++ src/main/java/KUSITMS/WITHUS/domain/application/application/enumerate/AcademicStatus.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum AcademicStatus {
     ENROLLED("재학"),
     GRADUATED("졸업"),
-    LEAVE_OF_ABSENCE("휴학");
+    LEAVE_OF_ABSENCE("휴학"),
+    DEFERRED("유예");
 
     private final String key;
 }
