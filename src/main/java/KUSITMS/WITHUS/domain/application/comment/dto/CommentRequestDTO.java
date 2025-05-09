a942bb2e53b8f4ec466da463d785260e31b54768
EunjinWoo <esther0904@naver.com>
2025-05-09 10:38:58 +0900

    chore: swagger dto 인식 설정 추가 & 포맷 통일 #67

--- src/main/java/KUSITMS/WITHUS/domain/application/comment/dto/CommentRequestDTO.java
+++ src/main/java/KUSITMS/WITHUS/domain/application/comment/dto/CommentRequestDTO.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 @Schema(description = "코맨트 관련 요청 DTO")
 public class CommentRequestDTO {
 
-    @Schema(name = "CommentRequestDTO.Create", description = "코맨트 생성 요청 DTO")
+    @Schema(description = "코맨트 생성 요청 DTO")
     public record Create(
             @Schema(description = "코맨트 내용", example = "큐시즘 경험 있음")
             @NotBlank String content,
@@ -17,7 +17,7 @@ public class CommentRequestDTO {
             @NotNull CommentType type
     ) {}
 
-    @Schema(name = "CommentRequestDTO.Update", description = "코맨트 수정 요청 DTO")
+    @Schema(description = "코맨트 수정 요청 DTO")
     public record Update (
             @Schema(description = "코맨트 내용", example = "큐시즘 경험 있음")
             @NotBlank String content
--- src/main/java/KUSITMS/WITHUS/domain/evaluation/evaluation/dto/EvaluationRequestDTO.java
+++ src/main/java/KUSITMS/WITHUS/domain/evaluation/evaluation/dto/EvaluationRequestDTO.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Schema(description = "평가 관련 요청 DTO")
 public class EvaluationRequestDTO {
 
-    @Schema(name = "EvaluationRequestDTO.Create", description = "평가 요청 DTO")
+    @Schema(description = "평가 요청 DTO")
     public record Create(
             @Schema(description = "지원서 ID", example = "1")
             @NotNull Long applicationId,
@@ -21,7 +21,7 @@ public class EvaluationRequestDTO {
             @NotNull int score
     ) {}
 
-    @Schema(name = "EvaluationRequestDTO.BulkCreate", description = "다중 평가 요청 DTO")
+    @Schema(description = "다중 평가 요청 DTO")
     public record BulkCreate(
             @Schema(description = "지원서 ID", example = "1")
             @NotNull Long applicationId,
@@ -30,7 +30,7 @@ public class EvaluationRequestDTO {
             @NotEmpty List<EvaluationItem> evaluations
 
     ) {
-        @Schema(name = "EvaluationRequestDTO.BulkCreate.EvaluationItem", description = "평가 항목 DTO")
+        @Schema(description = "평가 항목 DTO")
         public record EvaluationItem(
                 @Schema(description = "평가 기준 ID", example = "3")
                 @NotNull Long criteriaId,
