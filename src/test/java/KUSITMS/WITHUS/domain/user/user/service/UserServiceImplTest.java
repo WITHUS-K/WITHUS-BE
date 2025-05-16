2380b756321c1db0e7777ee1336bb8ac320a3f56
shash0423 <shash042319@naver.com>
2025-05-16 18:11:53 +0900

    test: SecurityTestUtil 테스트 #86

--- src/test/java/KUSITMS/WITHUS/domain/user/user/service/UserServiceImplTest.java
+++ src/test/java/KUSITMS/WITHUS/domain/user/user/service/UserServiceImplTest.java
@@ -1,22 +1,27 @@
 package KUSITMS.WITHUS.domain.user.user.service;
 
+import KUSITMS.WITHUS.domain.organization.organization.dto.OrganizationResponseDTO;
 import KUSITMS.WITHUS.domain.organization.organization.entity.Organization;
 import KUSITMS.WITHUS.domain.user.user.dto.UserRequestDTO;
+import KUSITMS.WITHUS.domain.user.user.dto.UserResponseDTO;
 import KUSITMS.WITHUS.domain.user.user.entity.User;
 import KUSITMS.WITHUS.global.common.enumerate.Gender;
 import KUSITMS.WITHUS.mock.container.TestContainer;
+import KUSITMS.WITHUS.util.SecurityTestUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserServiceImplTest {
 
     private TestContainer testContainer;
+    private User testUser;
 
     @BeforeEach
     void setUp() {
@@ -41,7 +46,8 @@ class UserServiceImplTest {
         );
 
         testContainer.userService.userJoinProcess(userJoinRequest);
-        // SecurityTestUtil.setAuthentication(this.testMember);
+        testUser = testContainer.userService.getUserByEmail(userJoinRequest.email());
+        SecurityTestUtil.setAuthentication(testUser);
     }
 
     @Test
@@ -58,5 +64,30 @@ class UserServiceImplTest {
         assertThat(user.getName()).isEqualTo("김재관");
         assertThat(user.getEmail()).isEqualTo("test@example.com");
         assertThat(user.getPhoneNumber()).isEqualTo("01012345678");
+    }
+
+    @Test
+    @DisplayName("마이페이지 조회 성공")
+    void getMyPageSuccessfully() {
+        // Given
+        // When
+        UserResponseDTO.MyPage result = testContainer.userService.getMyPage(testUser.getId());
+
+        // Then
+        assertThat(result.userId()).isEqualTo(testUser.getId());
+        assertThat(result.name()).isEqualTo(testUser.getName());
+        assertThat(result.phoneNumber()).isEqualTo(testUser.getPhoneNumber());
+        assertThat(result.email()).isEqualTo(testUser.getEmail());
+        assertThat(result.imageUrl()).isEqualTo(testUser.getProfileImageUrl());
+
+        List<Long> expectedOrgIds = testUser.getUserOrganizations().stream()
+                .map(userOrg -> userOrg.getOrganization().getId())
+                .toList();
+
+        List<Long> actualOrgIds = result.organizations().stream()
+                .map(OrganizationResponseDTO.Summary::id)
+                .toList();
+
+        assertThat(actualOrgIds).containsExactlyInAnyOrderElementsOf(expectedOrgIds);
     }
 }
