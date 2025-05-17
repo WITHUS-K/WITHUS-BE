b2befb926ddf6302b089025af6a8110f4353f04f
EunjinWoo <esther0904@naver.com>
2025-05-17 10:00:50 +0900

    fix: 로그인 시 응답값 추가 #90

--- src/main/java/KUSITMS/WITHUS/domain/user/user/repository/UserRepositoryImpl.java
+++ src/main/java/KUSITMS/WITHUS/domain/user/user/repository/UserRepositoryImpl.java
@@ -1,19 +1,24 @@
 package KUSITMS.WITHUS.domain.user.user.repository;
 
+import KUSITMS.WITHUS.domain.organization.organization.entity.QOrganization;
+import KUSITMS.WITHUS.domain.organization.organizationRole.entity.QOrganizationRole;
+import KUSITMS.WITHUS.domain.user.user.entity.QUser;
 import KUSITMS.WITHUS.domain.user.user.entity.User;
+import KUSITMS.WITHUS.domain.user.userOrganizationRole.entity.QUserOrganizationRole;
 import KUSITMS.WITHUS.global.exception.CustomException;
 import KUSITMS.WITHUS.global.exception.ErrorCode;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
     private final UserJpaRepository userJpaRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public Boolean existsByEmail(String email) {
@@ -51,5 +56,25 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public List<User> findAllById(List<Long> userIds) {
         return userJpaRepository.findAllById(userIds);
+    }
+
+    @Override
+    public User getByEmailWithOrgRoles(String email) {
+        QUser user = QUser.user;
+        QUserOrganizationRole userOrganizationRole = QUserOrganizationRole.userOrganizationRole;
+        QOrganizationRole organizationRole = QOrganizationRole.organizationRole;
+        QOrganization organization = QOrganization.organization;
+
+        User result = queryFactory
+                .selectFrom(user)
+                .leftJoin(user.userOrganizationRoles, userOrganizationRole).fetchJoin()
+                .leftJoin(userOrganizationRole.organizationRole, organizationRole).fetchJoin()
+                .leftJoin(organizationRole.organization, organization).fetchJoin()
+                .where(user.email.eq(email))
+                .fetchOne();
+        if (result == null) {
+            throw new CustomException(ErrorCode.USER_NOT_EXIST);
+        }
+        return result;
     }
 }
