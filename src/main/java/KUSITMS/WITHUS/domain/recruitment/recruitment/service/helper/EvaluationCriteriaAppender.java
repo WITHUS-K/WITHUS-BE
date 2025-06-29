389eb5e497130ee1144466bd6ded39173380b583
shash0423 <shash042319@naver.com>
2025-06-29 22:06:03 +0900

    fix: 평가 기준 파트별 저장 #116

--- src/main/java/KUSITMS/WITHUS/domain/recruitment/recruitment/service/helper/EvaluationCriteriaAppender.java
+++ src/main/java/KUSITMS/WITHUS/domain/recruitment/recruitment/service/helper/EvaluationCriteriaAppender.java
@@ -3,10 +3,14 @@ package KUSITMS.WITHUS.domain.recruitment.recruitment.service.helper;
 import KUSITMS.WITHUS.domain.evaluation.evaluationCriteria.dto.EvaluationCriteriaRequestDTO;
 import KUSITMS.WITHUS.domain.evaluation.evaluationCriteria.entity.EvaluationCriteria;
 import KUSITMS.WITHUS.domain.evaluation.evaluationCriteria.enumerate.EvaluationType;
+import KUSITMS.WITHUS.domain.recruitment.position.entity.Position;
 import KUSITMS.WITHUS.domain.recruitment.recruitment.entity.Recruitment;
+import KUSITMS.WITHUS.global.exception.CustomException;
+import KUSITMS.WITHUS.global.exception.ErrorCode;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class EvaluationCriteriaAppender {
@@ -19,14 +23,33 @@ public class EvaluationCriteriaAppender {
         if (criteriaList == null) return;
 
         for (EvaluationCriteriaRequestDTO.Create dto : criteriaList) {
+            Position position = getPositionIfExistsByName(dto.positionName(), recruitment);
+
+            boolean duplicate = recruitment.getEvaluationCriteriaList().stream().anyMatch(c ->
+                    c.getEvaluationType() == type &&
+                            c.getContent().equals(dto.content()) &&
+                            Objects.equals(c.getPosition(), position)
+            );
+            if (duplicate) continue;
 
             EvaluationCriteria criteria = EvaluationCriteria.builder()
                     .content(dto.content())
                     .description(dto.description())
                     .evaluationType(type)
+                    .position(position)
+                    .recruitment(recruitment)
                     .build();
 
             recruitment.addEvaluationCriteria(criteria);
         }
+    }
+
+    private Position getPositionIfExistsByName(String positionName, Recruitment recruitment) {
+        if (positionName == null) return null;
+
+        return recruitment.getPositions().stream()
+                .filter(p -> p.getName().equals(positionName))
+                .findFirst()
+                .orElseThrow(() -> new CustomException(ErrorCode.POSITION_NOT_EXIST));
     }
 }
